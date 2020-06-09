2126f40b930d12133ddc22b3b32a32191222de19
fixten <fixten@yandex.ru>
2020-06-09 18:43:58 +0300

    write sumbit test

--- src/components/Notes/New/Form/index.test.ts
+++ src/components/Notes/New/Form/index.test.ts
@@ -1,6 +1,23 @@
 import { submit } from './index';
 
-it('', () => {
+describe('create new note', () => {
   const onClose = jest.fn();
-  submit({ onClose }, { title: '', text: '' }, () => {});
+  const dispatch = jest.fn();
+  it('if no data', () => {
+    expect.assertions(2);
+    submit({ onClose }, { title: '', text: '' }, dispatch);
+    expect(onClose).toHaveBeenCalledTimes(0);
+    expect(dispatch).toHaveBeenCalledTimes(0);
+  });
+  it('with data', () => {
+    expect.assertions(2);
+    submit({ onClose }, { title: 'title', text: 'test' }, dispatch);
+    expect(onClose).toHaveBeenCalledTimes(1);
+    expect(dispatch).toHaveBeenCalledTimes(1);
+  });
+  it('test', () => {
+    expect.hasAssertions();
+    onClose();
+    expect(onClose).toHaveBeenCalledTimes(1);
+  });
 });
